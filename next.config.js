7a390891e722c4e20c6ca0c53fa29dabeac2ddd0
naimurk <naimurk781@gmail.com>
2024-10-23 01:47:32 +0600

    deploy issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+
+
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
+  }
+   
+  module.exports = nextConfig

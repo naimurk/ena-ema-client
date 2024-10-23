fe79b4c0c2cdfcf3b468bccfc31f73415c777eb3
naimurk <naimurk781@gmail.com>
2024-10-23 14:30:32 +0600

    base api

--- src/app/redux/api/baseApi.ts
+++ src/app/redux/api/baseApi.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: "https://ena-ema.vercel.app/api",
-  baseUrl: "http://localhost:8080/api",
+  baseUrl: "https://ena-ema.vercel.app/api",
+  // baseUrl: "http://localhost:8080/api",
 });
 export const baseApi = createApi({
   reducerPath: "baseApi",
